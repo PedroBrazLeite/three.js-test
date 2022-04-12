adb6a3b61e6dd05e18e3739b3bad8d13b402e11f
Pedro_Leite <50267170+PedroBrazLeite@users.noreply.github.com>
2022-04-12 13:59:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
